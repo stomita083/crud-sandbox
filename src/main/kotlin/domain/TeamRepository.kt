fe3cf2fe75685edc3583601f737db12618621a1b
stomita083 <s-tomita@unext.jp>
2021-02-12 22:18:10 +0900

    change to use join

--- src/main/kotlin/domain/TeamRepository.kt
+++ src/main/kotlin/domain/TeamRepository.kt
@@ -1,5 +1,6 @@
 package com.sora.data.crudsandbox.domain
 
+import com.sora.data.crudsandbox.domain.generated.tables.Member
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
@@ -11,11 +12,13 @@ import com.sora.data.crudsandbox.domain.generated.tables.pojos.Team as TeamVo
 class TeamRepository(private val dslContext: DSLContext) {
 
     val teamDb: TeamDb = TeamDb.TEAM
+    val memberDb: Member = Member.MEMBER
 
     fun findAll(): List<Team> {
         val selected: List<TeamVo> = dslContext
-            .select()
+            .select(teamDb.TEAM_ID, teamDb.NAME, teamDb.TYPE, teamDb.VERSION)
             .from(teamDb)
+            .join(memberDb).on(memberDb.TEAM_ID.eq(teamDb.TEAM_ID))
             .fetchInto(TeamVo::class.java)
 
         return selected
